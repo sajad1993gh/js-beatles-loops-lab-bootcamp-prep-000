c94f4c8c874944cc1ba9f416aafd5a0d036e518c
Sajad Gholamzadehrizi <sajad1993gh@gmail.com>
2019-01-14 21:31:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,12 +6,7 @@ function theBeatlesPlay(musicians,instruments){
   }
   return array
 }
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
+
 function johnLennonFacts(facts){
   var array=[]
   var i=0;
